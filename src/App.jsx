4a889073a61f8f71e23ceadf980c118bfba7c84f
unknown <smartkelvin66@gmail.com>
2024-02-19 14:34:15 +0100

    updated-react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline   text-red-500">
-        Hello World!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
